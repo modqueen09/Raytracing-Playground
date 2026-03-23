d0c6e6e1db1e27e2f99e49cd386eedb23f5535a1
Rori <sprewit1@jaguarlandrover.com>
2026-03-23 21:46:06 +0000

    12.2. Positioning and Orienting The Camera

--- main.cc
+++ main.cc
@@ -15,7 +15,7 @@ int main() {
     auto material_ground = make_shared<lambertian>(colour(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(colour(0.1, 0.2, 0.5));
     auto material_left   = make_shared<dielectric>(1.50);
-    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);    
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = make_shared<metal>(colour(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
@@ -26,10 +26,16 @@ int main() {
 
     camera cam;
 
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 400;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth = 50;
+    cam.max_depth         = 50;
+
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(-2,2,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
 
     cam.render(world);
 }
