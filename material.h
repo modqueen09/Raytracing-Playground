48b2c06be1074d23d47e337a18cb00aed12672c7
Rori <sprewit1@jaguarlandrover.com>
2026-03-23 21:33:49 +0000

    10.6. Fuzzy Reflection

--- material.h
+++ material.h
@@ -37,18 +37,20 @@ class lambertian : public material {
 
 class metal : public material {
   public:
-    metal(const colour& albedo) : albedo(albedo) {}
+    metal(const colour& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
     bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered)
     const override {
         vec3 reflected = reflect(r_in.direction(), rec.normal);
+        reflected = unit_vector(reflected) + (fuzz * random_unit_vector());
         scattered = ray(rec.p, reflected);
         attenuation = albedo;
-        return true;
+        return (dot(scattered.direction(), rec.normal) > 0);
     }
 
   private:
     colour albedo;
+    double fuzz;
 };
 
 #endif
