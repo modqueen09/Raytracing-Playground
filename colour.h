ab9522320fde1fdad1754235ec11a6fa6c2cd2ea
Rori <sprewit1@jaguarlandrover.com>
2026-03-23 21:21:31 +0000

    9.5. Using Gamma Correction for Accurate Colour Intensity

--- colour.h
+++ colour.h
@@ -6,17 +6,30 @@
 
 using colour = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 void write_colour(std::ostream& out, const colour& pixel_colour) {
     auto r = pixel_colour.x();
     auto g = pixel_colour.y();
     auto b = pixel_colour.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
     int rbyte = int(256 * intensity.clamp(r));
     int gbyte = int(256 * intensity.clamp(g));
     int bbyte = int(256 * intensity.clamp(b));
-    
+
     // Write out the pixel colour components.
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
