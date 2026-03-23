444c8a39c1457fafc3c9d5f40e5028727b2e5567
Rori <sprewit1@jaguarlandrover.com>
2026-03-23 21:26:05 +0000

    10.3. Modeling Light Scatter and Reflectance

--- material.h
+++ material.h
@@ -14,4 +14,25 @@ class material {
     }
 };
 
+class lambertian : public material {
+  public:
+    lambertian(const colour& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered)
+    const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+
+        // Catch degenerate scatter direction
+        if (scatter_direction.near_zero())
+            scatter_direction = rec.normal;
+
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    colour albedo;
+};
+
 #endif
